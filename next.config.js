999c66515dc16995f335fbb1770bea97afce7cd2
Pavarit Puangsrithiti <wosgateway@gmail.com>
2026-08-27 20:14:34 +0700

    fix: disable next image transformations

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ const withNextIntl = createNextIntlPlugin('./src/i18n/request.ts');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    unoptimized: true,
     dangerouslyAllowSVG: true,
     contentDispositionType: 'inline',
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
